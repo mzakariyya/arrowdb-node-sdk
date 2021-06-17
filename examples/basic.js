13020a7fd8aa7d4cdb2fbde29d9e35fa64bf5ce0
mzakariyya <mzakariyya@axway.com>
2021-06-17 12:21:00 +0100

    Update

--- examples/basic.js
+++ examples/basic.js
@@ -10,28 +10,32 @@ console.log('Creating ArrowDB app instance...'.cyan);
 var arrowDBApp = new ArrowDB(process.env.ARROWDB_APPKEY);
 console.log('Created: '.cyan + '%j', arrowDBApp);
 
-console.log('User logging in...'.cyan);
-arrowDBApp.userLogin({
-	login: 'paul',
-	password: 'cocoafish'
+
+
+console.log('User creating...'.cyan);
+arrowDBApp.userCreate({
+		'_login': 'paul',
+		'_password': 'cocoafish'
 }, function(err, result) {
 	if (err) {
 		console.error(err);
 		return;
 	}
+	console.log('User create request finished: '.cyan + '%j', result.body);
+	console.log('User logging in...'.cyan);
+	arrowDBApp.userLogin({
+		login: 'paul',
+		password: 'cocoafish'
+	}, function(err, result) {
+	if (err) {
+		console.error(err);
+	return;
+	}
 	console.log('User login request finished: '.cyan + '%j', result.body);
+	});
+
+	
 });
 
-// console.log('User creating...'.cyan);
-// arrowDBApp.userCreate({
-// 		'[{_login': 'paul1',
-// 		'_password': 'cocoafish1}]'
-// }, function(err, result) {
-// 	if (err) {
-// 		console.error(err);
-// 		return;
-// 	}
-// 	console.log('User create request finished: '.cyan + '%j', result.body);
-// });
 
 
