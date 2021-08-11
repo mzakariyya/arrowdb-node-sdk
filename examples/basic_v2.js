5223d0fc4a10c9b6f8c4e61a19565a8bd8a962b5
mzakariyya <mzakariyya@axway.com>
2021-08-11 23:08:20 +0100

    clean up

--- examples/basic_v2.js
+++ examples/basic_v2.js
@@ -14,86 +14,68 @@ var arrowDBApp = new ArrowDB(process.env.ARROWDB_APPKEY, {
 
 console.log('Created: '.cyan + '%j', arrowDBApp);
 
-//console.log('User creating...'.cyan);
-// arrowDBApp.userCreate({
-// 		'_login': 'paul2',
-// 		'_password': 'cocoafish2',
-// 		'_admin_': true,
-// }, function(err, resultCreate) {
-// 	if (err) {
-// 		console.error(err);
-// 		return;
-// 	}
-// 	console.log('User create request finished: '.cyan + '%j', resultCreate.body);
-// 	console.log('User logging in using SDK method...'.cyan);
-// 	var obj = JSON.parse(JSON.stringify(resultCreate.body));
-// 	userID = obj.response.data[0]._id;
+var username = 'paul2';
+var password = 'cocoafish2';
+var authNew = 'Basic ' + Buffer.from(username + ':' + password).toString('base64');
 
-	var username = 'paul2';
-	var password = 'cocoafish2';
-	var authNew = 'Basic ' + Buffer.from(username + ':' + password).toString('base64');
+arrowDBApp.userLogin({
+    req: {
+        headers: {
+            Authorization: authNew,
+        }
+    }
+}, function(err, resultLogin) {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log('User login request using SDK method finished: '.cyan + '%j', resultLogin.body);
+    console.log('Cookie string returned: %s', resultLogin.cookieString);
 
-	arrowDBApp.userLogin({
-		req: {
-			headers: {
-				Authorization: authNew,
-			}
-		}
-	}, function(err, resultLogin) {
-		if (err) {
-			console.error(err);
-			return;
-		}
-		console.log('User login request using SDK method finished: '.cyan + '%j', resultLogin.body);
-		console.log('Cookie string returned: %s', resultLogin.cookieString);
+    sessionCookieString = resultLogin.cookieString;   
+    let r = (Math.random() + 1).toString(36).substring(7);     
+    arrowDBApp.userCreate({
+        '_login': r,
+        '_password': 'cocoafish6',
+    }, function(err, resultCreate) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log('User create normal request finished: '.cyan + '%j', resultCreate.body);
+        var obj = JSON.parse(JSON.stringify(resultCreate.body));
+        userID = obj.response.data[0]._id;
 
-        sessionCookieString = resultLogin.cookieString;   
-        let r = (Math.random() + 1).toString(36).substring(7);     
-        arrowDBApp.userCreate({
-            '_login': r,
-            '_password': 'cocoafish6',
-        }, function(err, resultCreate) {
+        arrowDBApp.userQuery({
+            where: {"_id":userID},
+        }, function(err, resultQuery) {
             if (err) {
                 console.error(err);
                 return;
             }
-            console.log('User create normal request finished: '.cyan + '%j', resultCreate.body);
-            var obj = JSON.parse(JSON.stringify(resultCreate.body));
-            userID = obj.response.data[0]._id;
+            console.log('User query normal request finished: '.cyan + '%j', resultQuery.body);
 
-            arrowDBApp.userQuery({
-                where: {"_id":userID},
-            }, function(err, resultQuery) {
+            console.log('User update request using REST method: '.cyan);
+            arrowDBApp.put('/v2/user/'+userID, 
+            {
+                data: `{"$set":{"color":"blue"}}`,
+            },function(err, resultUpdateREST) {
                 if (err) {
                     console.error(err);
+                    console.log("user id is ", userID);
+                    arrowDBApp.userDelete({
+                        'user_id': userID,
+                    }, function(err, resultDelete) {
+                        if (err) {
+                            console.error(err);
+                            return;
+                        }
+                        console.log('User delete request finished: '.cyan + '%j', resultDelete.body);
+                    });	
                     return;
                 }
-                console.log('User query normal request finished: '.cyan + '%j', resultQuery.body);
-
-                console.log('User update request using REST method: '.cyan);
-                arrowDBApp.put('/v2/user/'+userID, 
-                {
-                    data: `{"$set":{"color":"blue"}}`,
-                },function(err, resultUpdateREST) {
-                    if (err) {
-                        console.error(err);
-                        console.log("user id is ", userID);
-                        arrowDBApp.userDelete({
-                            'user_id': userID,
-                        }, function(err, resultDelete) {
-                            if (err) {
-                                console.error(err);
-                                return;
-                            }
-                            console.log('User delete request finished: '.cyan + '%j', resultDelete.body);
-                        });	
-                        return;
-                    }
-                    console.log('User update request finished: '.cyan + '%j', resultUpdateREST.body);
-                });	
-            });		
-        });
-	});
-
-// });
-
+                console.log('User update request finished: '.cyan + '%j', resultUpdateREST.body);
+            });	
+        });		
+    });
+});
